b3671260f380fb3d878a9e5b79912001e29cbf05
Henry Lee <lihongtao988@gmail.com>
2011-12-20 09:26:20 +0800

    Bugfix: Some phone should register receiver in AndroidManifest.xml

--- src/com/cn6000/callrec/AutoRunReceiver.java
+++ src/com/cn6000/callrec/AutoRunReceiver.java
@@ -3,13 +3,44 @@ package com.cn6000.callrec;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class AutoRunReceiver extends BroadcastReceiver {
 	@Override
-	public void onReceive(Context paramContext, Intent paramIntent) {
+	public void onReceive(Context context, Intent intent) {
+		if (null == intent)
+			return;
 		Intent i = new Intent(CallRecorderService.ACTION);
-		paramContext.startService(i);
-		Log.d(CallRecorderService.TAG, "call recorder service starting...");
+		if (Intent.ACTION_NEW_OUTGOING_CALL.equals(intent.getAction())) {
+			i.putExtra(CallRecorderService.STATE, CallRecorderService.OUTGOING);
+			i.putExtra(Intent.EXTRA_PHONE_NUMBER,
+					intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER));
+		} else if (TelephonyManager.ACTION_PHONE_STATE_CHANGED.equals(intent
+				.getAction())) {
+			String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
+			if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
+				i.putExtra(CallRecorderService.STATE,
+						CallRecorderService.INCOMING);
+				i.putExtra(Intent.EXTRA_PHONE_NUMBER, intent
+						.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+			} else if (TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)) {
+				i.putExtra(CallRecorderService.STATE, CallRecorderService.BEGIN);
+			} else if (TelephonyManager.EXTRA_STATE_IDLE.equals(state)) {
+				i.putExtra(CallRecorderService.STATE, CallRecorderService.END);
+			}
+		} else if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+			i.putExtra(CallRecorderService.STATE, CallRecorderService.START);
+		} else if (intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)
+				|| intent.getAction().equals(Intent.ACTION_MEDIA_REMOVED)) {
+			i.putExtra(CallRecorderService.STATE, CallRecorderService.STORAGE);
+		} else {
+			return;
+		}
+		context.startService(i);
+		Log.d(CallRecorderService.TAG,
+				"AutoRunReceiver startService "
+						+ i.getStringExtra(CallRecorderService.STATE) + ":"
+						+ i.getStringExtra(Intent.EXTRA_PHONE_NUMBER));
 	}
 }
--- src/com/cn6000/callrec/CallRecorderService.java
+++ src/com/cn6000/callrec/CallRecorderService.java
@@ -5,22 +5,27 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Service;
-import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.database.Cursor;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Environment;
 import android.os.IBinder;
 import android.provider.ContactsContract.PhoneLookup;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class CallRecorderService extends Service {
 	public static final String ACTION = "com.cn6000.callrec.CALL_RECORD";
+	public static final String STATE = "STATE";
+	public static final String START = "START";
+	public static final String STORAGE = "STORAGE";
+	public static final String INCOMING = "INCOMING";
+	public static final String OUTGOING = "OUTGOING";
+	public static final String BEGIN = "BEGIN";
+	public static final String END = "END";
+
 	protected static final String TAG = CallRecorderService.class.getName();
 	private static final String AMR_DIR = "/callrec/";
 	private static final String IDLE = "";
@@ -29,7 +34,6 @@ public class CallRecorderService extends Service {
 
 	private Context cntx;
 	private volatile String fileNamePrefix = IDLE;
-	private volatile BroadcastReceiver receiver;
 	private volatile MediaRecorder recorder;
 	private volatile boolean isMounted = false;
 	private volatile boolean isInRecording = false;
@@ -43,26 +47,45 @@ public class CallRecorderService extends Service {
 	public void onCreate() {
 		// TODO Auto-generated method stub
 		super.onCreate();
-		cntx = getApplicationContext();
+		this.cntx = getApplicationContext();
+		this.prepareAmrDir();
 	}
 
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "service destory");
 		this.stopRecording();
-		this.unregisterReceiver();
-		cntx = null;
+		this.cntx = null;
 		super.onDestroy();
 	}
 
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		if (null != intent && ACTION.equals(intent.getAction())) {
-			Log.d(TAG, "service start");
-			prepareAmrDir();
-			registerReceiver();
+		if (null == intent || !ACTION.equals(intent.getAction())) {
+			return super.onStartCommand(intent, flags, startId);
 		}
-		return super.onStartCommand(intent, flags, startId);
+		String state = intent.getStringExtra(STATE);
+		if (OUTGOING.equals(state)) {
+			String phoneNo = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+			fileNamePrefix = getContactName(this.getContext(), phoneNo)
+					+ OUTGOING_CALL_SUFFIX;
+		} else if (INCOMING.equals(state)) {
+			String phoneNo = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
+			fileNamePrefix = getContactName(this.getContext(), phoneNo)
+					+ INCOMING_CALL_SUFFIX;
+		} else if (BEGIN.equals(state)) {
+			startRecording();
+		} else if (END.equals(state)) {
+			stopRecording();
+		} else if (STORAGE.equals(state)) {
+			String mountState = Environment.getExternalStorageState();
+			if (Environment.MEDIA_MOUNTED.equals(mountState)) {
+				prepareAmrDir();
+			} else {
+				isMounted = false;
+			}
+		}
+		return START_STICKY;
 	}
 
 	public Context getContext() {
@@ -91,111 +114,24 @@ public class CallRecorderService extends Service {
 			return;
 		stopRecording();
 		try {
-			Log.d(TAG, "prepare MediaRecorder");
-			recorder = new MediaRecorder();
-			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-			recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-			recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 			File amr = new File(Environment.getExternalStorageDirectory()
 					.getAbsolutePath()
 					+ AMR_DIR
 					+ getDateTimeString()
 					+ "_"
 					+ fileNamePrefix + ".amr");
+			Log.d(TAG, "Prepare recording in " + amr.getAbsolutePath());
+			recorder = new MediaRecorder();
+			recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+			recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+			recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 			recorder.setOutputFile(amr.getAbsolutePath());
 			recorder.prepare();
 			recorder.start();
 			isInRecording = true;
-			Log.d(TAG, "call recording in file " + amr.getAbsolutePath());
+			Log.d(TAG, "Recording in " + amr.getAbsolutePath());
 		} catch (Exception e) {
 			Log.w(TAG, e);
-		}
-	}
-
-	private void onExternalStorageStateChange(Context context, Intent intent) {
-		String state = Environment.getExternalStorageState();
-		if (Environment.MEDIA_MOUNTED.equals(state)) {
-			isMounted = true;
-		} else {
-			isMounted = false;
-		}
-	}
-
-	private void onOutgoingCall(Context context, Intent intent, String extra) {
-		String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-		String phoneNo = extra;
-		if (null == phoneNo) {
-			phoneNo = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
-		}
-		Log.d(TAG,
-				"Outgoing call, extra_phone_number: "
-						+ phoneNo
-						+ " extra_incoming_number: "
-						+ intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
-						+ " State: " + state);
-		fileNamePrefix = getContactName(context, phoneNo) + OUTGOING_CALL_SUFFIX;
-		Log.d(TAG, fileNamePrefix);
-	}
-
-	private void onPhoneStateChange(Context context, Intent intent) {
-		String state = intent.getStringExtra(TelephonyManager.EXTRA_STATE);
-		Log.d(TAG,
-				"Phone state changed, extra_phone_number: "
-						+ intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
-						+ " extra_incoming_number: "
-						+ intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
-						+ " State: " + state);
-		if (TelephonyManager.EXTRA_STATE_OFFHOOK.equals(state)) {
-			Log.d(TAG, "offhook, start recording...");
-			startRecording();
-		} else if (TelephonyManager.EXTRA_STATE_IDLE.equals(state)) {
-			Log.d(TAG, "idle, stop recording...");
-			stopRecording();
-			fileNamePrefix = IDLE;
-			Log.d(TAG, fileNamePrefix);
-		} else if (TelephonyManager.EXTRA_STATE_RINGING.equals(state)) {
-			Log.d(TAG, "ringing, book incoming number...");
-			fileNamePrefix = getContactName(
-					context,
-					intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER))
-					+ INCOMING_CALL_SUFFIX;
-			Log.d(TAG, fileNamePrefix);
-		}
-	}
-
-	private void registerReceiver() {
-		receiver = new BroadcastReceiver() {
-			@Override
-			public void onReceive(Context context, Intent intent) {
-				if (null == intent) {
-					throw new NullPointerException("intent can not null");
-				}
-				Log.d(TAG, intent.getAction());
-				if (intent.getAction().equals(Intent.ACTION_MEDIA_MOUNTED)
-						|| intent.getAction().equals(
-								Intent.ACTION_MEDIA_REMOVED)) {
-					onExternalStorageStateChange(context, intent);
-				} else if (intent.getAction().equals(
-						Intent.ACTION_NEW_OUTGOING_CALL)) {
-					onOutgoingCall(context, intent, getResultData());
-				} else if (intent.getAction().equals(
-						TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
-					onPhoneStateChange(context, intent);
-				}
-			}
-		};
-		IntentFilter filter = new IntentFilter();
-		filter.addAction(Intent.ACTION_MEDIA_MOUNTED);
-		filter.addAction(Intent.ACTION_MEDIA_REMOVED);
-		filter.addAction(Intent.ACTION_NEW_OUTGOING_CALL);
-		filter.addAction(TelephonyManager.ACTION_PHONE_STATE_CHANGED);
-		registerReceiver(receiver, filter);
-	}
-
-	private void unregisterReceiver() {
-		if (null != receiver) {
-			unregisterReceiver(receiver);
-			receiver = null;
 		}
 	}
 
@@ -211,6 +147,7 @@ public class CallRecorderService extends Service {
 	}
 
 	private String getContactName(Context cntx, String phoneNo) {
+		if (null == phoneNo) return "";
 		Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI,
 				Uri.encode(phoneNo));
 		ContentResolver cr = cntx.getContentResolver();
